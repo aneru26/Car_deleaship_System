d039900066904194683798f47fe716abbbc9cc9d
Arnel P. Tagupa <arneltagupa1431@gmail.com>
2024-01-11 00:10:40 +0800

    first commit

---
+++ app/Models/Purchase.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Purchase extends Model
+{
+    use HasFactory;
+
+    protected $table ="purchases";
+
+    static public function getSingle($id) 
+    {
+        return self::find($id);
+    }
+
+    static public function getRecord()
+    {
+       
+
+        $return = Purchase::select('purchases.*','cars.vin','cars.brands','cars.models','cars.photo','users.name as name', 'users.last_name as last_name')
+                ->join('cars', 'cars.id', '=', 'purchases.car_id')
+                ->join('users', 'users.id', '=', 'purchases.customer_id')
+                ->where('purchases.is_delete','=', 0)
+                ->orderBy('purchases.id','desc')
+                ->paginate(10);
+
+        return $return;
+    }
+
+    static public function getTotalPurchaseCar()
+    {
+       
+
+        $return = Purchase::select('purchases.*','cars.vin','cars.brands','cars.models','cars.photo','users.name as name', 'users.last_name as last_name')
+                ->join('cars', 'cars.id', '=', 'purchases.car_id')
+                ->join('users', 'users.id', '=', 'purchases.customer_id')
+                ->where('purchases.is_delete','=', 0)
+                ->orderBy('purchases.id','desc')
+                ->count();
+
+        return $return;
+    }
+
+    static public function getcustomerRecord()
+    {
+
+
+        $return = Purchase::select('purchases.*','cars.*','users.name as name', 'users.last_name as last_name')
+                ->join('cars', 'cars.id', '=', 'purchases.car_id')
+                ->join('users', 'users.id', '=', 'purchases.customer_id')
+                ->where('purchases.is_delete','=', 0)
+                ->orderBy('purchases.id','desc')
+                ->paginate(10);
+
+        return $return;
+    }
+
+    
+    static public function getTotalPuchaseCustomer()
+    {
+
+
+        $return = Purchase::select('purchases.*','cars.*','users.name as name', 'users.last_name as last_name')
+                ->join('cars', 'cars.id', '=', 'purchases.car_id')
+                ->join('users', 'users.id', '=', 'purchases.customer_id')
+                ->where('purchases.is_delete','=', 0)
+                ->orderBy('purchases.id','desc')
+                ->count();
+
+        return $return;
+    }
+
+    public function getPhotoDirect()
+    {
+        if(!empty($this->photo) && file_exists('upload/car/'.$this->photo))
+        {
+            return url('upload/car/'.$this->photo);
+        }
+        else
+        {
+            return asset('upload/profile/user.jpg');
+        
+        }
+    
+    }
+}
